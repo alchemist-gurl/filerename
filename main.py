7a2fe63fc47a59e41af27830f3802018c2024e7c
Autumn Laniel <autumn.rose1205@gmail.com>
2018-07-03 13:31:02 -0700

    loop,  rename.  still not working

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
 def rename_files():
     file_list = os.listdir("/Users/autumnlaniel/Documents/Programming/PythonPractice/filerename/idk files")
-    print(file_list)
+    saved_path = os.getcwd()
+    #print(file_list)
+    os.chdir("/Users/autumnlaniel/Documents/Programming/PythonPractice/filerename/idk files")
+
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate("0123456789") )
+    os.chdir(saved_path)
 rename_files()
