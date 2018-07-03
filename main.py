2894eeb5932f5ff9af788eeb115a42b83bcb0268
Autumn Laniel <autumn.rose1205@gmail.com>
2018-07-03 13:57:35 -0700

    fixed

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 def rename_files():
     file_list = os.listdir("/Users/autumnlaniel/Documents/Programming/PythonPractice/filerename/idk files")
     saved_path = os.getcwd()
-    #print(file_list)
+    print(file_list)
     os.chdir("/Users/autumnlaniel/Documents/Programming/PythonPractice/filerename/idk files")
 
     for file_name in file_list:
-        os.rename(file_name, file_name.translate("0123456789") )
+        os.rename(file_name, file_name.translate(str.maketrans(dict.fromkeys("0123456789"))))
     os.chdir(saved_path)
 rename_files()
+
+
